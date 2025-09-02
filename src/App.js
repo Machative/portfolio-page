88736d9ec217601e9d0a05b58cddd720c1124ef2
Machative <machjayus@gmail.com>
2025-09-02 11:41:21 -0400

    Main template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ProjectList from './components/ProjectList';
+import Project_IoTLog from './pages/Project_IoTLog';
+import Project_EBike from './pages/Project_EBike';
+import Project_RCSwitch from './pages/Project_RCSwitch';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectList />} />
+          <Route path="/projects/IoTLoggingNetwork" element={<Project_IoTLog />} />
+          <Route path="/projects/SelfDrivingBicycle" element={<Project_EBike />} />
+          <Route path="/projects/GPRCSwitch" element={<Project_RCSwitch />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
